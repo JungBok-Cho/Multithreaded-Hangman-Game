d7c08b3dc2ab53288c7bbac7ede3ce39c5e98344
JungBok-Cho <55774376+JungBok-Cho@users.noreply.github.com>
2020-06-11 18:36:22 -0700

    Modify the code in connection.h

--- server/connection.h
+++ server/connection.h
@@ -11,86 +11,93 @@
 #include <iostream>
 
 class connection {
+	
 public:
-	/**
-	 * This default constructor is not used.
-	 *
-	 *
-	 * INPUT: N/A
-	 * OUTPUT: N/A
-	*/
+    /**
+     * This default constructor is not used.
+     *
+     * INPUT: N/A
+     * OUTPUT: N/A
+     */
     connection();
 
-	/**
-	 * This constructor creates a connection which stores all relevant info for the connection
-	 *
-	 *
-	 * INPUT: int socket
-	 * INPUT: int reference to server_fd 
-	 * INPUT: sockaddr_in struct reference for address info
-	 * OUTPUT: connection object set up with connection information
-	*/
+	
+    /**
+     * This constructor creates a connection which stores all relevant info for the connection
+     *
+     * INPUT: int socket
+     * INPUT: int reference to server_fd 
+     * INPUT: sockaddr_in struct reference for address info
+     * OUTPUT: connection object set up with connection information
+     */
     connection(int mySocket, int &server_fd, struct sockaddr_in &address);
 
-	/**
-	 * This sends a given string to the computer that the connection info points to
-	 * optionally it will then listen for a response.
-	 *
-	 *
-	 * INPUT: string RPC to send
-	 * INPUT: bool should I wait for a response?
-	 * OUTPUT: response from recipient of message 
-	 *         (only if bRead is true, else it will just dump the 
-	 *          input buffer or return null)
-	*/
+	
+    /**
+     * This sends a given string to the computer that the connection info points to
+     * optionally it will then listen for a response.
+     *
+     * INPUT: string RPC to send
+     * INPUT: bool should I wait for a response?
+     * OUTPUT: response from recipient of message 
+     *         (only if bRead is true, else it will just dump 
+     *          the input buffer or return null)
+     */
     std::string sendRPC(std::string RPC, bool bRead);
 
-	/**
-	 * This function instructs the connection to wait and listen until it gets
-	 * a message from the connected computer.
-	 *
-	 *
-	 * OUTPUT: string response message from connected computer
-	*/
+	
+    /**
+     * This function instructs the connection to wait and listen until it gets
+     * a message from the connected computer.
+     *
+     * OUTPUT: string response message from connected computer
+     */
     std::string listen();
 
-	/**
-	 * This function instructs the connection to send a disconnect message
-	 * then delete itself, effectively closing the connection.
-	*/
+	
+    /**
+     * This function instructs the connection to send a disconnect message
+     * then delete itself, effectively closing the connection.
+     */
     void end();
 
-	/**
-	 * This function instructs the connection to close it's socket
-	 * It may be safely reassigned to a different socket at this point
-	*/
-	void closeSocket();
+	
+    /**
+     * This function instructs the connection to close it's socket
+     * It may be safely reassigned to a different socket at this point
+     */
+    void closeSocket();
 
-	// Setter for socket number
-	void setSocket(int newSocket);
+	
+    // Setter for socket number
+    void setSocket(int newSocket);
 
-	// Getter for socket number
-	int getSocket();
 
-	// Setter for thread number
-	void setThreadNum(int num);
+    // Getter for socket number
+    int getSocket();
 
+
+    // Setter for thread number
+    void setThreadNum(int num);
+
+	
     // Getter for thread number
     int getThreadNum();
 
+	
 private:
-	///private variables
+    ///private variables
 
-	//thread number the connection is operating on
+    //thread number the connection is operating on
     int threadNum;
 
-	//socket the connection is operating on
+    //socket the connection is operating on
     int mySocket;
 
-	//server info for the connection
+    //server info for the connection
     int server_fd;
 
-	//struct for holding the addressing info for the connection
+    //struct for holding the addressing info for the connection
     struct sockaddr_in address;
 };
 
