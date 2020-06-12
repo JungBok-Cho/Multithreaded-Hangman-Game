77f7a697e4ba2230ea301a0e2ad28e1310964aa5
JungBok-Cho <55774376+JungBok-Cho@users.noreply.github.com>
2020-06-11 18:38:05 -0700

    Modify the code in game.cpp

--- server/game.cpp
+++ server/game.cpp
@@ -17,28 +17,28 @@ void game::reload(const std::string& word) {
 std::string game::step(char c) {
     std::string rsp;
 
-	std::string newMask = this->mask;
-	for (int i = 0; i < (int) word.length(); i++) {
-		if (word[i] == c) {
-			newMask[i] = c;
-		}
+    std::string newMask = this->mask;
+    for (int i = 0; i < (int) word.length(); i++) {
+    	if (word[i] == c) {
+		newMask[i] = c;
 	}
+    }
 
-	if (newMask == mask) {
-		score--;
-	} else {
-		this->mask = newMask;
-	}
+    if (newMask == mask) {
+	score--;
+    } else {
+	this->mask = newMask;
+    }
 
     if(word == mask) {
-		this->totalScore += this->score;
+	this->totalScore += this->score;
         rsp = ("type=gameend;score=" + std::to_string(this->score) + ";word=" + this->word
                 + ";result=" + "win;totalScore=" + std::to_string(this->totalScore) + ";");
     } else if(score <= 0) {
         rsp = ("type=gameend;score=" + std::to_string(this->score) + ";word=" + this->word
                 + ";result=" + "fail;totalScore=" + std::to_string(this->totalScore) + ";");
     } else {
-		rsp = ("type=gaming;mask=" + this->mask + ";score=" + std::to_string(this->score) + ";");
+	rsp = ("type=gaming;mask=" + this->mask + ";score=" + std::to_string(this->score) + ";");
     }
 
     return rsp;
